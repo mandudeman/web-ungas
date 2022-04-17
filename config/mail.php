cf10db671b1142f3825b9b5dc6727c171a0def7d
Shift <shift@laravelshift.com>
2022-04-17 06:26:59 +0000

    Apply Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-2 coding style as a base with some minor additions.
    
    You may customize the code style applied by adding a [PHP CS Fixer][1] or [PHP CodeSniffer][2] ruleset to your project root. Feel free to use [Shift's Laravel ruleset][3] to help you get started.
    
    For more information on customizing the code style applied by Shift, [watch this short video][4].
    
    [1]: https://github.com/FriendsOfPHP/PHP-CS-Fixer
    [2]: https://github.com/squizlabs/PHP_CodeSniffer
    [3]: https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200
    [4]: https://laravelshift.com/videos/shift-code-style

--- config/mail.php
+++ config/mail.php
@@ -10,7 +10,7 @@ return [
     | This option controls the default mailer that is used to send any email
     | messages sent by your application. Alternative mailers may be setup
     | and used as needed; however, this mailer will be used by default.
-    | 
+    |
     |
     */
 
@@ -89,7 +89,6 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
-    
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
